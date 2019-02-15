8be42504b3d0c311d6936ca3b4d4a9624131c9aa
Florian Uhlig <f.uhlig@gsi.de>
2019-02-15 14:19:42 +0100

    Update versions

--- scripts/package_versions.sh
+++ scripts/package_versions.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-export CBMEXTERNALS_VERSION=feb19
+export CBMEXTERNALS_VERSION=may18
 
 export FAIRSOFT_LOCATION="https://github.com/FairRootGroup/FairSoft"
 export FAIRSOFT_VERSION=may18
+
+export VC_LOCATION="https://github.com/VcDevel/Vc.git"
+export VC_TAG=1.4.1
+
+export MILLEPEDE_LOCATION="http://svnsrv.desy.de/public/MillepedeII/tags/"
+export MILLEPEDE_VERSION=V04-03-10
 
 
